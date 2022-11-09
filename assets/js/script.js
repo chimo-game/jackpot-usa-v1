98bab72d944e84ed5e58d534e3e53f3cf5e1f929
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-09 20:34:25 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+  notificationToast.classList.add('closed');
+});
+
+// this script below i found by myself
+
+let time = Math.round((Math.random()) + 2);
+document.getElementById("time").innerHTML = time + ' Minutes ago';
+
+let time2 = Math.round((Math.random()) + 2);
+document.getElementById("time2").innerHTML = time2 + ' Minutes ago';
+
+let time3 = Math.round((Math.random()) + 2);
+document.getElementById("time3").innerHTML = time3 + ' Minutes ago';
+
+let d = Math.round((Math.random() * 50) + 2);
+document.getElementById("dollar").innerHTML = '$' + d;
+
+let d2 = Math.round((Math.random() * 50) + 2);
+document.getElementById("dollar2").innerHTML = '$' + d2;
+
+let d3 = Math.round((Math.random() * 50) + 2);
+document.getElementById("dollar3").innerHTML = '$' + d3;
+
+
+
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
