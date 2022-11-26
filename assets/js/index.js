726a3ad2b087863e737c2981323b6020a097e704
tolapao <62795957+XANuN@users.noreply.github.com>
2022-11-27 00:00:16 +0700

    added

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
  * GLOBAL VARS
  * -------------------------- */
 // The date you want to count down to
-var targetDate = new Date("2022/11/26 00:00:00");
+var targetDate = new Date("2022/12/01 00:00:00");
 
 // Other date related variables
 var days;
